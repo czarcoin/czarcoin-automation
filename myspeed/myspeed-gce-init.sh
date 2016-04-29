693cfbf00d8ea93d8d56a41e15048097fe30c80e
Philip Hutchins <philip@storj.io>
2016-04-29 12:46:46 -0400

    fixing download of init script

--- myspeed/myspeed-gce-init.sh
+++ myspeed/myspeed-gce-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git
+apt-get install -y git htop
 useradd -m -d /opt/storj storj
 sudo su - storj -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash"
 sudo su - storj -c "source ~/.nvm/nvm.sh && nvm install stable && nvm use stable && nvm alias default stable"
-sudo su - storj -c "git clone https://github.com/gordonwritescode/myspeed.git && cd myspeed && npm install"
+sudo su - storj -c "source ~/.nvm/nvm.sh && git clone https://github.com/gordonwritescode/myspeed.git && cd myspeed && npm install"
 mkdir /var/log/storj
 chown storj:storj /var/log/storj
-curl -o /etc/init/myspeed.conf https://github.com/storj/storj-automation/raw/master/myspeed/init/myspeed.conf
+wget -P /etc/init https://github.com/storj/storj-automation/raw/master/myspeed/init/myspeed.conf
 service myspeed start
