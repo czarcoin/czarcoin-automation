029601862cf310418388b89aab527957918a4955
Philip Hutchins <philip@storj.io>
2016-04-29 16:13:30 -0400

    fixing iptables install

--- myspeed/myspeed-gce-init.sh
+++ myspeed/myspeed-gce-init.sh
@@ -8,8 +8,8 @@ sudo su - storj -c "source ~/.nvm/nvm.sh && nvm install stable && nvm use stable
 sudo su - storj -c "source ~/.nvm/nvm.sh && git clone https://github.com/gordonwritescode/myspeed.git && cd myspeed && npm install"
 mkdir /var/log/storj
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get install -y iptables-persistent
 chown storj:storj /var/log/storj
 wget -P /etc/init https://github.com/storj/storj-automation/raw/master/myspeed/init/myspeed.conf
