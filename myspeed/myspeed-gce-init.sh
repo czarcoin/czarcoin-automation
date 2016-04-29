bca3933dc6149d90f501af59800ad18f67d1aca3
Philip Hutchins <philip@storj.io>
2016-04-29 13:17:55 -0400

    adding forwarding rule for 80 to 8080

--- myspeed/myspeed-gce-init.sh
+++ myspeed/myspeed-gce-init.sh
@@ -7,6 +7,10 @@ sudo su - storj -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0
 sudo su - storj -c "source ~/.nvm/nvm.sh && nvm install stable && nvm use stable && nvm alias default stable"
 sudo su - storj -c "source ~/.nvm/nvm.sh && git clone https://github.com/gordonwritescode/myspeed.git && cd myspeed && npm install"
 mkdir /var/log/storj
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get install -y iptables-persistent
 chown storj:storj /var/log/storj
 wget -P /etc/init https://github.com/storj/storj-automation/raw/master/myspeed/init/myspeed.conf
 service myspeed start
