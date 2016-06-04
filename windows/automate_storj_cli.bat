5bb70e6d9a1f9ee2b3b09b9de16d70b4e0eaf5f1
computeronix <computeronix@users.noreply.github.com>
2016-06-03 21:00:13 -0400

    Update automate_storj_cli.bat
    Added powershell file checking

--- windows/automate_storj_cli.bat
+++ windows/automate_storj_cli.bat
@@ -3,5 +3,13 @@ REM ****************************************
 REM Storj-cli Windows Automation Menu
 REM ****************************************
 
+SET install=%~dp0install_storj_cli.ps1
+
+IF NOT EXIST "%install%" (
+	ECHO File %install% Does Not Exist
+	PAUSE
+	EXIT
+)
+
 REM Run install_storj_cli.ps1 Power Shell Script
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0install_storj_cli.ps1""' -Verb RunAs}"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%install%""' -Verb RunAs}"
