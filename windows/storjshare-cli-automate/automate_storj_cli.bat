e52309c852359308520e8adb1305e623dc973023
computeronix <computeronix@users.noreply.github.com>
2016-06-09 08:28:36 -0400

    Update automate_storj_cli.bat

--- windows/storjshare-cli-automate/automate_storj_cli.bat
+++ windows/storjshare-cli-automate/automate_storj_cli.bat
@@ -11,5 +11,5 @@ IF NOT EXIST "%install%" (
 	EXIT
 )
 
-REM Run install_storj_cli.ps1 Power Shell Script
+REM Run automate_storj_cli.ps1 Power Shell Script
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass ""%install%""' -Verb RunAs}"
