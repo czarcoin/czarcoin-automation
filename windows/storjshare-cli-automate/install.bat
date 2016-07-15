398376ec0050852c32f7c987b35b8d472cc0858d
computeronix <computeronix@users.noreply.github.com>
2016-07-15 00:08:07 -0400

    4.0 Release
    --removed -noninteractive to allow for the password to be entered during the service installation part

--- windows/storjshare-cli-automate/install.bat
+++ windows/storjshare-cli-automate/install.bat
@@ -13,4 +13,4 @@ IF NOT EXIST "%install%" (
 
 REM Run automate_storj_cli.ps1 Power Shell Script
 PowerShell.exe -NoProfile -Command "& {Unblock-File '%install%'}"
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -NoLogo -NonInteractive -NoExit -WindowStyle Normal -ExecutionPolicy Bypass & {&''%install%''}' -Verb RunAs}"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -NoLogo -NoExit -WindowStyle Normal -ExecutionPolicy Bypass & {&''%install%''}' -Verb RunAs}"
