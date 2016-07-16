1e29775642165f24267773e8fd218b1f6f29bf6c
computeronix <computeronix@users.noreply.github.com>
2016-07-16 12:27:56 -0400

    Create install.bat

---
+++ windows/storj-automate/install.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+REM ****************************************
+REM Storj Windows Automation Menu
+REM ****************************************
+
+SET install=%~dp0automate_storj.ps1
+
+IF NOT EXIST "%install%" (
+	ECHO File %install% Does Not Exist
+	PAUSE
+	EXIT
+)
+
+REM Run Power Shell Script
+PowerShell.exe -NoProfile -Command "& {Unblock-File '%install%'}"
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -NoLogo -NoExit -WindowStyle Normal -ExecutionPolicy Bypass & {&''%install%''}' -Verb RunAs}"
