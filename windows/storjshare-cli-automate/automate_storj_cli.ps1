cffe12e40d6cf78afd06398f3daf001aa7b3aebf
computeronix <computeronix@users.noreply.github.com>
2016-10-15 11:59:04 -0400

    Update automate_storj_cli.ps1
    Updated 5.3
    
    Removed cached downloads and attempt a re-download of them as needed

--- windows/storjshare-cli-automate/automate_storj_cli.ps1
+++ windows/storjshare-cli-automate/automate_storj_cli.ps1
@@ -159,7 +159,7 @@ param(
 
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
-$global:script_version="5.2" # Script version
+$global:script_version="5.3" # Script version
 $global:reboot_needed=""
 $global:noupnp=""
 $global:installsvc="true"
@@ -1173,30 +1173,31 @@ function Get-ProgramVersion([string]$program) {
 
 function DownloadFile([string]$url, [string]$targetFile) {
     if((Test-Path $targetFile)) {
-        LogWrite "$targetFile exists, using this download";
-    } else {
-        $uri = New-Object "System.Uri" "$url"
-        $request = [System.Net.HttpWebRequest]::Create($uri)
-        $request.set_Timeout(15000) #15 second timeout
-        $response = $request.GetResponse()
-        $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-        $responseStream = $response.GetResponseStream()
-        $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
-        $buffer = new-object byte[] 10KB
-        $count = $responseStream.Read($buffer,0,$buffer.length)
-        $downloadedBytes = $count
-        while ($count -gt 0) {
-            $targetStream.Write($buffer, 0, $count)
-            $count = $responseStream.Read($buffer,0,$buffer.length)
-            $downloadedBytes = $downloadedBytes + $count
-            Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
-        }
-        Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
-        $targetStream.Flush()
-        $targetStream.Close()
-        $targetStream.Dispose()
-        $responseStream.Dispose()
+        LogWrite "$targetFile exists, removing it and re-downloading it";
+        Remove-Item $targetFile
     }
+
+    $uri = New-Object "System.Uri" "$url"
+    $request = [System.Net.HttpWebRequest]::Create($uri)
+    $request.set_Timeout(15000) #15 second timeout
+    $response = $request.GetResponse()
+    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+    $responseStream = $response.GetResponseStream()
+    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+    $buffer = new-object byte[] 10KB
+    $count = $responseStream.Read($buffer,0,$buffer.length)
+    $downloadedBytes = $count
+    while ($count -gt 0) {
+        $targetStream.Write($buffer, 0, $count)
+        $count = $responseStream.Read($buffer,0,$buffer.length)
+        $downloadedBytes = $downloadedBytes + $count
+        Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+    }
+    Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+    $targetStream.Flush()
+    $targetStream.Close()
+    $targetStream.Dispose()
+    $responseStream.Dispose()
 }
 
 function FollowDownloadFile([string]$url, [string]$targetFile) {
